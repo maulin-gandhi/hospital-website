316359e58b730ff5b3963f46fa5f7051a779d610
Maulin Gandhi <maulingithub@gmail.com>
2026-03-30 11:50:25 +0530

    remove fallback route fix pages

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ const app = express();
 // Serve static files
 app.use(express.static(path.join(__dirname, 'Public')));
 
-// Default route
+// Home route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public/HTML/index.html'));
-});
-
-// FIXED fallback route
-app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'Public/HTML/index.html'));
 });
 
