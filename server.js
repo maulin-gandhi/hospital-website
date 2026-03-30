8cd65670efa53c92ce49545359439e270b623aa9
Maulin Gandhi <maulingithub@gmail.com>
2026-03-30 19:54:08 +0530

    file file explicit routes

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const path = require('path');
 
 const app = express();
 
-// Serve HTML folder directly
-app.use(express.static(path.join(__dirname, 'Public/HTML')));
+// Serve entire Public folder
+app.use(express.static(path.join(__dirname, 'Public')));
+
+// Explicit routes (IMPORTANT FIX)
+app.get('/about.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/HTML/about.html'));
+});
+
+app.get('/services.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/HTML/services.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/HTML/contact.html'));
+});
+
+app.get('/appointment.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public/HTML/Appointment.html'));
+});
 
 // Home route
 app.get('/', (req, res) => {
