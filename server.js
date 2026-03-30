d5d9d3b9fcff1bfde9c1a0f5df8a9d863cb14b45
Maulin Gandhi <maulingithub@gmail.com>
2026-03-30 20:07:42 +0530

    fixed folder structure final

--- server.js
+++ server.js
@@ -3,30 +3,7 @@ const path = require('path');
 
 const app = express();
 
-// Serve entire Public folder
 app.use(express.static(path.join(__dirname, 'Public')));
-
-// Explicit routes (IMPORTANT FIX)
-app.get('/about.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public/HTML/about.html'));
-});
-
-app.get('/services.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public/HTML/services.html'));
-});
-
-app.get('/contact.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public/HTML/contact.html'));
-});
-
-app.get('/appointment.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public/HTML/Appointment.html'));
-});
-
-// Home route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Public/HTML/index.html'));
-});
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
