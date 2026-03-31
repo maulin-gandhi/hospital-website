27ab3c94e8b7109402dce9218f5dc92e8dbc1f3f
Maulin Gandhi <maulingithub@gmail.com>
2026-03-31 18:02:38 +0530

    Final login debug

--- server.js
+++ server.js
@@ -121,6 +121,9 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
+    console.log("INPUT USER:", username);
+    console.log("INPUT PASS:", password);
+
     console.log("ENV USER:", process.env.ADMIN_USER_1);
     console.log("ENV PASS:", process.env.ADMIN_PASS_1);
 
